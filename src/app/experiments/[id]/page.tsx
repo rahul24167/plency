b3ca03415ac84aef3c3de8d146c972b438abb888
Rahul Choudhary <rahul24167@gmail.com>
2025-06-04 16:30:28 +0530

    Alignment fixed and Pages for the individual project added

--- src/app/experiments/[id]/page.tsx
+++ src/app/experiments/[id]/page.tsx
@@ -13,7 +13,7 @@ const Project = ({ params }: ProjectPageProps) => {
       "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
       "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+      "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
     ],
     title: "Project Title",
     brand: "Brand Name",
@@ -21,12 +21,16 @@ const Project = ({ params }: ProjectPageProps) => {
   };
   return (
     <div className="h-screen w-screen overflow-auto flex flex-col flex-wrap p-4 py-8 pb-[15vh] gap-2">
-
       <div className="w-1/2 h-[90vh] flex flex-col justify-end pb-1/4">
-        <h1 className="text-largest text-secondary font-bold uppercase">{projectInfo.title}</h1>
-        <h1 className="text-largest text-secondary font-bold uppercase">{projectInfo.brand}</h1>
-        <h3 className="text-medium text-secondary font-bold uppercase">{projectInfo.service}</h3>
-
+        <h1 className="text-largest text-secondary font-bold uppercase">
+          {projectInfo.title}
+        </h1>
+        <h1 className="text-largest text-secondary font-bold uppercase">
+          {projectInfo.brand}
+        </h1>
+        <h3 className="text-medium text-secondary font-bold uppercase">
+          {id}{projectInfo.service}
+        </h3>
       </div>
       <div className="w-[calc(50%-4px)] h-[90vh] overflow-auto">
         <div className="w-full h-full border my-2"></div>
@@ -44,7 +48,6 @@ const Project = ({ params }: ProjectPageProps) => {
         <div className="w-full h-full  my-2 border "></div>
         <div className="w-full h-full  my-2 border "></div>
         <div className="w-full h-full  my-2 border "></div>
-      
       </div>
     </div>
   );
