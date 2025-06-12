c97b31a011f100cf2d19653711b08abb05ac3245
Rahul Choudhary <rahul24167@gmail.com>
2025-06-12 18:37:13 +0530

    New homepage text animation added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,46 @@
-
 import { Variants } from "motion/react";
-import * as motion from "motion/react-client"
-const subHeading:string = "BUILDING BRIDGES BETWEEN BRANDS AND PEOPLE."
+import * as motion from "motion/react-client";
+const subHeading: string = "BUILDING BRIDGES BETWEEN BRANDS AND PEOPLE.";
 const childVariants: Variants = {
-  initial: { opacity: 0, y: 10 },
-  visible: { opacity: 1, y: 0 },
+  initial: { y: "100%" },
+  visible: { y: "0%" },
 };
 const parentVariants: Variants = {
   initial: { backgroundPositionY: "50%" },
   visible: { backgroundPositionY: "0%" },
 };
 export default function Home() {
-
+  const subHeadingArray: string[] = subHeading.split(" ");
   return (
     <motion.div
       variants={parentVariants}
       style={{
-         backgroundPositionX: "center", // Lock X so only Y moves
+        backgroundPositionX: "center", // Lock X so only Y moves
         backgroundPositionY: "50%", // fallback
       }}
-      transition={{delay:0.2, duration: 0.5, ease: "easeInOut" }}
+      transition={{ delay: 0.2, duration: 0.5, ease: "easeInOut" }}
       className="h-screen w-full bg-[url('/bgGradient.png')] bg-no-repeat"
       initial="initial"
-      whileHover="visible" 
+      whileHover="visible"
     >
-      <motion.div
-        variants={childVariants}
-        transition={{delay: 0.2, duration: 0.5, ease: "easeInOut" }}
-        className="px-5 pt-6 h-1/2 w-full text-medium md:text-large lg:text-largest font-bold"
-      >
-        {subHeading}
-      </motion.div>
+      <div className="flex flex-wrap px-5 pt-6 w-full">
+        {subHeadingArray.map((word, index) => (
+          <span key={index} className="overflow-hidden inline-block">
+            <motion.span
+              variants={childVariants}
+              transition={{
+                delay: 0.2,
+                duration: 0.5,
+                ease: "easeInOut",
+              }}
+              className="text-medium md:text-large lg:text-largest font-bold inline-block"
+            >
+              {word}&nbsp;
+            </motion.span>
+          </span>
+        ))}
+      </div>
 
-      <div className="h-1/2 w-full" />
     </motion.div>
   );
 }
--- src/components/customCursor/index.tsx
+++ src/components/customCursor/index.tsx
@@ -5,7 +5,7 @@ export default function CustomCursor() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
-    const moveHandler = (e:MouseEvent) => {
+    const moveHandler = (e: MouseEvent) => {
       setPosition({ x: e.clientX, y: e.clientY });
     };
 
@@ -15,10 +15,15 @@ export default function CustomCursor() {
 
   return (
     <>
-
       <div
-        className=" w-25 h-25 rounded-full bg-gray-500 blur-2xl fixed pointer-events-none shadow-lg shadow-gray-500 transition-transform duration-75 ease-linear "
+        className="w-[40vh] h-[40vh] rounded-full fixed pointer-events-none transition-transform duration-75 ease-linear opacity-80 blur-3xl"
         style={{
+          background:
+            "radial-gradient(circle, rgba(255,255,255,0.5) 0%, rgba(255,255,255,0) 70%)",
+          boxShadow: `
+            0 0 60px 20px rgba(255, 255, 255, 0.4),
+            0 0 120px 40px rgba(255, 255, 255, 0.3),
+            0 0 180px 60px rgba(255, 255, 255, 0.2)`,
           transform: `translate(${position.x}px, ${position.y}px) translate(-50%, -50%)`,
         }}
       ></div>
