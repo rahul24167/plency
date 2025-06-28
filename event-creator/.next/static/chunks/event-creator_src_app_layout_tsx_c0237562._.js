2f198c0237b5b0da6387874fb7cd3ca528befbff
Rahul Kumar Choudhary <113711057+rahul24167@users.noreply.github.com>
2025-06-28 19:29:05 +0530

    Delete event-creator/.next directory

--- event-creator/.next/static/chunks/event-creator_src_app_layout_tsx_c0237562._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/event-creator_src_app_723fc3b2._.css"
-],
-    source: "dynamic"
-});
