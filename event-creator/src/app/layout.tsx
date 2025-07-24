ca0da4c19983de079e3b792f646a8d6d37dd3162
Rahul Choudhary <rahul24167@gmail.com>
2025-07-25 03:50:41 +0530

    font issue fixed in the event creator

--- event-creator/src/app/layout.tsx
+++ event-creator/src/app/layout.tsx
@@ -7,7 +7,7 @@ const readexPro = localFont({
   display: 'swap',
   src: [
     {
-      path: "../../../public/fonts/ReadexPro-VariableFont_HEXP,wght.ttf",
+      path: "./fonts/ReadexPro-VariableFont_HEXP,wght.ttf",
     },
   ],
   variable: '--font-readex-pro',
