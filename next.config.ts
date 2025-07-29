8cb3613db6ef1fe7de9dc5449225562931ee0ba4
Rahul Choudhary <rahul24167@gmail.com>
2025-07-29 16:15:22 +0530

    unoptimized removed

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    remotePatterns: [new URL('https://cdn.plency.com/**')],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.plency.com',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
